abefe8d74f89cfb0772dc32b4b759a4aa3fd855b
Andreas Fuchs <asf@boinkor.net>
2024-03-05 08:50:20 -0500

    When cargo-watch rebuilds, have it tell us why

--- flake.nix
+++ flake.nix
@@ -122,7 +122,7 @@
 
         proc.groups.dev-server.processes = {
           backend.command = ''
-            ${pkgs.cargo-watch}/bin/cargo-watch -i *.nix -i lz-frontend -- cargo run --color always --bin lz-web -- --db dev-db.sqlite --authentication-header-name X-Tailscale-User-LoginName --default-user-name=developer --listen-on=127.0.0.1:3000
+            ${pkgs.cargo-watch}/bin/cargo-watch --why -i *.nix -i lz-frontend -- cargo run --color always --bin lz-web -- --db dev-db.sqlite --authentication-header-name X-Tailscale-User-LoginName --default-user-name=developer --listen-on=127.0.0.1:3000
           '';
           sqlite-web = {
             command = "${pkgs.sqlite-web}/bin/sqlite_web -x -r dev-db.sqlite";

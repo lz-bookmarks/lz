bc69f09a57eb1e794017d4334eb7aae1a5a98d8a
Andreas Fuchs <asf@boinkor.net>
2024-03-04 21:39:37 -0500

    Make it generate pretty JSON
    There's only so much pain we can bear

--- src/lz-web/src/bin/generate-openapi-specs.rs
+++ src/lz-web/src/bin/generate-openapi-specs.rs
@@ -1,6 +1,6 @@
 use utoipa::OpenApi;
 
 fn main() -> Result<(), anyhow::Error> {
-    println!("{}", lz_web::api::ApiDoc::openapi().to_json()?);
+    println!("{}", lz_web::api::ApiDoc::openapi().to_pretty_json()?);
     Ok(())
 }

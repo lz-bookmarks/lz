6b9c37a9b8dc402e09ebd6ce273a7faffde8edd8
Andreas Fuchs <asf@boinkor.net>
2024-03-31 21:52:08 -0400

    Define all tables as STRICT
    That ensures that strings can't be stored in integer columns (??!?!)
    
    But: it also means that the things we *thought* were types (JSON,
    DATETIME, BOOLEAN) - aren't. We have to specify them as the types that
    sqlite stores them as, and then do more type juggling. Thankfully sqlx
    is strict, but ugh.

--- src/lz-db/migrations/20240225141910_initial_schema.sql
+++ src/lz-db/migrations/20240225141910_initial_schema.sql
@@ -3,39 +3,39 @@
 CREATE TABLE "users" (
   "user_id" INTEGER NOT NULL PRIMARY KEY,
   "name" TEXT NOT NULL UNIQUE,
-  "created_at" datetime NOT NULL
-);
+  "created_at" TEXT NOT NULL
+) STRICT;
 
 CREATE UNIQUE INDEX "users_by_name" ON "users" ("name");
 
 CREATE TABLE "bookmarks" (
   "bookmark_id" INTEGER NOT NULL PRIMARY KEY,
   "user_id" INTEGER NOT NULL,
-  "created_at" datetime NOT NULL,
-  "modified_at" datetime,
-  "accessed_at" datetime,
+  "created_at" TEXT NOT NULL,
+  "modified_at" TEXT,
+  "accessed_at" TEXT,
   "url" TEXT NOT NULL,
   "title" TEXT NOT NULL,
   "description" TEXT,
   "website_title" TEXT,
   "website_description" TEXT,
   "notes" TEXT,
-  "unread" BOOLEAN,
-  "shared" BOOLEAN,
-  "import_properties" JSON,
+  "unread" INTEGER,
+  "shared" INTEGER,
+  "import_properties" TEXT,
 
   FOREIGN KEY ("user_id") REFERENCES "users"("user_id")
-);
+) STRICT;
 
 CREATE UNIQUE INDEX "bookmarks_by_user_and_url" ON "bookmarks" ("user_id", "url");
 
 CREATE TABLE "tags" (
   "tag_id" INTEGER NOT NULL PRIMARY KEY,
-  "created_at" datetime NOT NULL,
+  "created_at" TEXT NOT NULL,
   "name" TEXT NOT NULL UNIQUE,
 
   CHECK("name" NOT LIKE '% %' AND length("name") >= 1)
-);
+) STRICT;
 
 CREATE UNIQUE INDEX "tags_by_name" ON "tags" ("name");
 
@@ -46,4 +46,4 @@ CREATE TABLE "bookmark_tags" (
   PRIMARY KEY ("bookmark_id","tag_id"),
   FOREIGN KEY("bookmark_id") REFERENCES "bookmarks"("bookmark_id"),
   FOREIGN KEY("tag_id") REFERENCES "tags"("tag_id")
-);
+) STRICT;

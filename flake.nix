36a569eef4f8e3569dc0cd1f265a595ffd77a304
Andreas Fuchs <asf@boinkor.net>
2024-04-10 10:07:01 -0400

    Configure djlint to auto-format templates

--- flake.nix
+++ flake.nix
@@ -162,6 +162,7 @@
               pkgs.sqlite
               pkgs.cargo-watch
               pkgs.nodejs_21
+              pkgs.djlint
               pkgs.nodePackages.typescript-language-server
               fenix.packages.${system}.targets.wasm32-unknown-unknown.stable.rust-std
             ];

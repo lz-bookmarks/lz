6f5ea9507fb4746f3916e3b1594ea19e2d1d5cb3
Andreas Fuchs <asf@boinkor.net>
2024-04-21 21:27:33 -0400

    Put the add link into the navbar

--- src/lz-ui/src/components/layout.rs
+++ src/lz-ui/src/components/layout.rs
@@ -22,9 +22,14 @@ pub fn layout(LayoutProps { children }: &LayoutProps) -> Html {
     html! {
         <ContextProvider<ModalState> context={*ctx}>
             <CreateForm onclose={{let ctx = ctx.clone(); move |_ev| ctx.set(ModalState::Normal)}} />
-            <button class="btn" onclick={{move |_ev| ctx.set(ModalState::CreateBookmark)}}>
-                { "add" }
-            </button>
+            <div class={classes!("navbar", "bg-base-100")}>
+                <div class="navbar-start" />
+                <div class="navbar-end">
+                    <button class="btn" onclick={{move |_ev| ctx.set(ModalState::CreateBookmark)}}>
+                        { "add" }
+                    </button>
+                </div>
+            </div>
             { children.clone() }
         </ContextProvider<ModalState>>
     }

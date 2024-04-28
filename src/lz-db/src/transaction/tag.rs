a77525944bee9d3e1a4de03ecbbe40f589bbb9a9
Steve Cook <snarkout@gmail.com>
2024-04-28 13:25:42 -0400

    Dropping pattern match on TagName schema

--- src/lz-db/src/transaction/tag.rs
+++ src/lz-db/src/transaction/tag.rs
@@ -74,7 +74,7 @@ impl From<&Tag<TagId>> for TagId {
 
 /// The name representation of a tag.
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone, ToSchema, ToResponse)]
-pub struct TagName(#[schema(min_length = 1, pattern = "^[a-z0-9:-]+$")] pub String);
+pub struct TagName(#[schema(min_length = 1)] pub String);
 
 impl AsRef<str> for TagName {
     fn as_ref(&self) -> &str {
